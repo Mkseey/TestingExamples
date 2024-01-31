e1a0a57ecb18ca6502d8fe6d5eccc3df4f7bee4c
Mkseey <kartikmahurkar752@gmail.com>
2024-01-31 15:25:37 +0530

    understanding the connection with the database using uri give custom port no.

--- D2/index.js
+++ D2/index.js
@@ -1,24 +1,27 @@
 const { MongoClient } = require('mongodb');
+// import express from "express";
+const express = require("express")
+// import http from "http";
+const http = require("http");
+
+const app = express();
 
 async function main() {
-  const uri = "mongodb://127.6.16.6:3050/"
+  const uri = "mongodb://127.0.0.1:27017"
+  let server = http.createServer(app);
   const client = new MongoClient(uri);
+  const PORT = 3060;
   try {
     const connected = await client.connect();
     if (connected) {
+      server.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
       console.log("the client is connected");
     }
-    await listDatabases(client);
   }
   catch (e) {
     console.log("error connecting the databases");
   }
 }
-
 main()
-
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
